1e6212bc60934064f2f43e993477e7995a2a126d
Lele <ladcunha@hotmail.com>
2019-10-16 22:36:28 -0300

    oi

---
+++ servidor.py
@@ -0,0 +1,42 @@
+#devemos utilizar semáforos pra fazer bloqueante??
+#criar um servidor http é fazer linda space com wrapper rest??
+
+import http.server
+import socketserver
+import os
+import sys
+import socket
+
+defPort = 8080
+
+#Definição do formato da entrada do micro-blog
+class Entrada(object):
+	"""docstring for Entrada"""
+	def __init__(self, topico, menssagem):
+		super(Entrada, self).__init__()
+		self.topico = arg
+		self.menssagem = 
+
+
+#Definição do servidor do micro-blog
+class Servidor(object):
+	"""docstring for Servidor"""
+	def __init__(self, PORT = defPort):
+		super(Servidor, self).__init__()
+		self.arg = arg
+
+
+def inicia(self):
+	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serversocket.bind(('localhost', 8080))
+	serversocket.listen(5)		#Numero maximo de clientes simultaneos 
+		
+	def atividade(comando):		#Definição do recebimento de atividade no servidor pelo cliente???
+			if comando == 'out':
+						
+			if comando == 'in' :
+
+			if comando == 'rd' :
+				
+			else:
+				return False

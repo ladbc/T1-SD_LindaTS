84e3f28e2b28638b24e5333effb5bffa3aa87705
Lele <ladcunha@hotmail.com>
2019-10-17 00:17:43 -0300

    oioioi

--- servidor.py
+++ servidor.py
@@ -1,5 +1,5 @@
-#devemos utilizar semáforos pra fazer bloqueante??
-#criar um servidor http é fazer linda space com wrapper rest??
+#devemos utilizar semaforos pra fazer bloqueante??
+#criar um servidor http e fazer linda space com wrapper rest??
 
 import http.server
 import socketserver
@@ -7,36 +7,32 @@ import os
 import sys
 import socket
 
-defPort = 8080
+porta   = 8080
+bufferSize  = 1024
+msgFromServer = "Cliente conectado ao servidor"
+bytesToSend = str.encode(msgFromServer)
 
-#Definição do formato da entrada do micro-blog
-class Entrada(object):
-	"""docstring for Entrada"""
-	def __init__(self, topico, menssagem):
-		super(Entrada, self).__init__()
-		self.topico = arg
-		self.menssagem = 
+def atividadeTS(comando): #Definicao do recebimento de atividade no servidor pelo cliente???
+	if comando == 'out':
+		print('OLHA AI MENINA')
+		return		
+	if comando == 'in' :
+		return
+	if comando == 'rd' :
+		return
+	else:
+		return False
 
+socketservidor = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)	# Cria o socket para comunicacao
+socketservidor.bind(('', porta))		# Une o cliente ao socket servidor
+#socketservidor.listen(5)
+print("Servidor disponivel")
 
-#Definição do servidor do micro-blog
-class Servidor(object):
-	"""docstring for Servidor"""
-	def __init__(self, PORT = defPort):
-		super(Servidor, self).__init__()
-		self.arg = arg
-
-
-def inicia(self):
-	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serversocket.bind(('localhost', 8080))
-	serversocket.listen(5)		#Numero maximo de clientes simultaneos 
-		
-	def atividade(comando):		#Definição do recebimento de atividade no servidor pelo cliente???
-			if comando == 'out':
-						
-			if comando == 'in' :
-
-			if comando == 'rd' :
-				
-			else:
-				return False
+while(True):
+    bytesAdrPar = socketservidor.recvfrom(bufferSize)
+    mensagem = bytesAdrPar[0]
+    adr = bytesAdrPar[1]
+    clientMsg = "Message from Client:{}".format(mensagem)
+    clientIP  = "Client IP Address:{}".format(adr)
+    atividadeTS(mensagem)
+    socketservidor.sendto(bytesToSend, adr)

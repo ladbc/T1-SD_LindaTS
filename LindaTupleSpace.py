631f4b8cce08f472fccc06c19598a501543381d5
Lele <ladcunha@hotmail.com>
2019-10-17 00:43:47 -0300

    aaa

--- LindaTupleSpace.py
+++ LindaTupleSpace.py
@@ -1,5 +1,5 @@
-#devemos utilizar semáforos pra fazer bloqueante??
-#criar um servidor http é fazer linda space com wrapper rest??
+#devemos utilizar semaforos pra fazer bloqueante??
+#criar um servidor http e fazer linda space com wrapper rest??
 
 import http.server
 import socketserver
@@ -9,26 +9,19 @@ import socket
 
 defPort = 8080
 
-#Definição do cliente do micro-blog   
-class Client():
-  def __init__(self, addr: tuple):
-      self.__socket = socket.socket()
-      self.__socket.connect(addr)
-
-  def close(self):
-      self.__socket.close()
-
-  def out_tuple(self, topico, menssagem):    #Operação de inserção no tuple space
-    #Postagem de conteúdo por tópicos
-    Servidor.atividade
-
-  def in_tuple(self, topico, menssagem):     #Operação de retirada no tuple space BLOQUEANTE
-    #Retirada de conteúdo somente por quem postou
-    print("ai")
-    
-  def rd_tuple(self, topico):     #Operação de cópia do template de tupla no tuple space BLOQUEANTE
-    #Obtenção de conteúdo por tópicos
-    print("ai")
+def close(self):
+	return
+def out_tuple(self, topico, menssagem):    #Operacao de insercao no tuple space
+  #Postagem de conteudo por topicos
+  return
+def in_tuple(self, topico, menssagem):     #Operacao de retirada no tuple space BLOQUEANTE
+  #Retirada de conteudo somente por quem postou
+  print("ai")
+  return
+def rd_tuple(self, topico):     #Operacao de copia do template de tupla no tuple space BLOQUEANTE
+  #Obtencao de conteudo por topicos
+  print("ai")
+  return
 
 mensagemcliente       = "out"
 bytesenviar         = str.encode(mensagemcliente)
